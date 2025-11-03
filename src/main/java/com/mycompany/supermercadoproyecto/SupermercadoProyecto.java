5a534cc281b5b573f1929e2f148d8b6bc0a9088c
mario <mar0065@alu.medac.es>
2025-11-03 13:33:03 +0100

    mi primer commit

---
+++ src/main/java/com/mycompany/supermercadoproyecto/SupermercadoProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermercadoproyecto;
+
+/**
+ *
+ * @author MEDAC
+ */
+public class SupermercadoProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
